27e0876e423d249bd0d2d18654c1e75d79b652e8
joemaxh <joemaxh@gmail.com>
2016-09-14 12:02:59 -0500

    -added sort feature to wishlist table -added delete item option for wishlist table

--- src/main/java/delGame.java
+++ src/main/java/delGame.java
@@ -5,22 +5,28 @@
  */
 
 import HibernateUtil.HibernateUtil;
+import com.mycompany.mavenproject1.SystemUser;
 import com.mycompany.mavenproject1.WishListItem;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 
 /**
  *
  * @author Max Huhman
  */
-public class fetchTable extends HttpServlet {
+@WebServlet(urlPatterns = {"/delGame"})
+public class delGame extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -35,24 +41,49 @@ public class fetchTable extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            String col = request.getParameter("col");
-            String by = request.getParameter("acdc");
-            List<WishListItem> list = createGameList(col, by);
-            request.setAttribute("wish", list);
-            request.getRequestDispatcher("/success.jsp").forward(request, response);
+            String errorMsg = "";
+            String title = request.getParameter("title");
+            if(getGame(title) != null){
+                WishListItem wli = getGame(title);
+                delete(wli);
+                RequestDispatcher rdp = request.getRequestDispatcher("/success.jsp");
+                rdp.forward(request, response);
+            } else{
+                //fail
+                errorMsg = "Could not find game";
+                request.setAttribute("errorMsg", errorMsg);
+                RequestDispatcher rdp = request.getRequestDispatcher("/fail.jsp");
+                rdp.forward(request, response);
+            }
+            
+            //forward to update servlet later
             
         }
     }
-    
-    public static List<WishListItem> createGameList(String col, String by){
+    public static WishListItem getGame(String title){
         SessionFactory sf = HibernateUtil.getSessionFactory();
         Session session = sf.openSession();
-        @SuppressWarnings("unchecked")
-        List<WishListItem> gameList;
-        gameList = session.createSQLQuery("SELECT * FROM wishlist ORDER BY "+ col +" "+ by).addEntity(WishListItem.class).list();
-        session.close();
-        return gameList;
+        Criteria crit = session.createCriteria(WishListItem.class);
+        Criterion cond1 = Restrictions.eq("title", title);
+        crit.add(Restrictions.and(cond1));
+        return (WishListItem) crit.uniqueResult();
     }
+    private static void delete(WishListItem wli){
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.delete(wli);
+        session.getTransaction().commit();
+        session.close();
+    }
+    private static WishListItem read(int id){
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        WishListItem su = (WishListItem) session.get(WishListItem.class, id);
+        session.close();
+        return su;
+    }
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
--- src/main/webapp/success.jsp
+++ src/main/webapp/success.jsp
@@ -26,8 +26,23 @@
         <form action="fetchManage" submit>
             <input class="submit" type="submit" value="Manage Account"/>
         </form>
+        <br>
         <form action="fetchTable" submit>
             <input class="submit" type="submit" value="Update Table"/>
+            <select name="col">
+                <option value="title">Title</option>
+                <option value="genre">Genre</option>
+                <option value="localp">Local Players</option>
+                <option value="onlinep">Online Players</option>
+                <option value="normalPrice">Normal Price</option>
+                <option value="salePrice">Sale Price</option>
+                <option value="savings">Savings (%)</option>
+                <option value="metacriticScore">Metacritic Score</option>
+            </select>
+            <select name="acdc">
+                <option value="ASC">Ascending Order</option>
+                <option value="DESC">Descending Order</option>
+            </select>
         </form>
         
         <table>
@@ -43,14 +58,18 @@
             </tr>
             <c:forEach items="${wish}" var="wish" >
                 <tr>
-                    <td><c:out value="${wish.title}"/></td>
-                    <td><c:out value="${wish.genre}"/></td>
-                    <td><c:out value="${wish.localp}"/></td>
-                    <td><c:out value="${wish.onlinep}"/></td>
-                    <td><c:out value="${wish.normalPrice}"/></td>
-                    <td><c:out value="${wish.salePrice}"/></td>
-                    <td><c:out value="${wish.savings}"/></td> 
-                    <td><c:out value="${wish.metacriticScore}"/></td>
+                    <form action="delGame" submit>
+                        <input type="hidden" name="title" value="${wish.title}">
+                        <td><c:out value="${wish.title}"/></td>
+                        <td><c:out value="${wish.genre}"/></td>
+                        <td><c:out value="${wish.localp}"/></td>
+                        <td><c:out value="${wish.onlinep}"/></td>
+                        <td><c:out value="${wish.normalPrice}"/></td>
+                        <td><c:out value="${wish.salePrice}"/></td>
+                        <td><c:out value="${wish.savings}"/></td> 
+                        <td><c:out value="${wish.metacriticScore}"/></td>
+                        <td><input class="submit" type="submit" value="Delete Item"/></td>
+                    </form>
                 </tr>
             </c:forEach>
         </table>
